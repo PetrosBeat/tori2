3fd8a48bbf533e06d9cde4c81900128db79b31f7
RzynaDev <garcias.aravena@gmail.com>
2021-07-03 19:30:35 -0400

    2.1

---
+++ application/logs/log-2021-07-03.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-07-03 19:07:41 --> 404 Page Not Found: Assets/fondo.jpg
+ERROR - 2021-07-03 19:08:22 --> 404 Page Not Found: Assets/js
+ERROR - 2021-07-03 19:14:40 --> 404 Page Not Found: Assets/js
+ERROR - 2021-07-03 19:15:09 --> 404 Page Not Found: Assets/js
